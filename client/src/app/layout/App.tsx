0d021fe3728209008eef944e98614f2fd3399600
hhyyy9@qq.com <huangyu@ivinsight.com>
2023-07-10 18:16:38 +0800

    add Header and Card components

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Typography } from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
+import Header from "./Header";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]
@@ -27,8 +28,11 @@ function App() {
 
   return (
     <>
-      <Typography variant="h1">DotNet Store</Typography>
-      <Catalog products={products} addProduct={addProduct} />
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} addProduct={addProduct} />
+      </Container>
     </>
   );
 }
