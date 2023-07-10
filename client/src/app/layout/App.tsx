62acf3b4e19cffbc3c4b565e4af15c93d4627c1c
hhyyy9@qq.com <huangyu@ivinsight.com>
2023-07-10 15:31:28 +0800

    import MUI style framework

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
+import { Typography } from "@mui/material";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]
@@ -25,10 +26,10 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>DotNet Store</h1>
+    <>
+      <Typography variant="h1">DotNet Store</Typography>
       <Catalog products={products} addProduct={addProduct} />
-    </div>
+    </>
   );
 }
 
