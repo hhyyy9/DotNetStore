58d5d2fb162615ea3d0a8ce3c2e99d100933e373
hhyyy9@qq.com <huangyu@ivinsight.com>
2023-07-27 15:15:59 +0800

    fix bug when remove items on basket page.

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -46,7 +46,7 @@ const requests = {
   get: (url: string) => axios.get(url).then(responseBody),
   post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
   put: (url: string, body: {}) => axios.get(url, body).then(responseBody),
-  delete: (url: string) => axios.get(url).then(responseBody),
+  delete: (url: string) => axios.delete(url).then(responseBody),
 };
 
 const Catalog = {
